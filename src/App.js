0b209a97daf9232ae69b529404dc4cfb5d299bcd
hungtqclone <121530023+hungtqclone@users.noreply.github.com>
2024-01-07 13:46:33 +0700

    setup file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Navigate, Outlet, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
+import TestLoadapi from './resoureces/components/test/TestLoadapi';
+import Login from './resoureces/components/users/Login';
+
 
 function App() {
+
+  //đọc thông tin user từ localStorage
+  const getUserFromLocalStorage = () => {
+    const userString = localStorage.getItem('user');
+    if (userString) {
+      return JSON.parse(userString);
+    }
+    return null
+  }
+
+  //Lưu thông tin user vào localStorage
+  const saveUserToLocalStorage = (userInfo) => {
+    if (!userInfo) {
+      localStorage.removeItem('user');
+      setUser(null);
+      return
+    }
+    localStorage.setItem('user', JSON.stringify(userInfo));
+    setUser(userInfo);
+  }
+  const [user, setUser] = useState(getUserFromLocalStorage);
+  const ProtectedRoute = () => {
+    if (user) {
+      return <Outlet />
+    }
+    return <Navigate to="/login" />
+  }
+  const PublicRoute = () => {
+    if (user) {
+      return <Navigate to="/" />
+    }
+    return <Outlet />
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router>
+        <Routes>
+          <Route element={<PublicRoute />}>
+            {/* màn hình đăng nhập, quên mật khẩu... ở đây */}
+            <Route path="/login" element={<Login saveUser={saveUserToLocalStorage} />} />
+          </Route>
+          <Route element={<ProtectedRoute />}>
+            {/* sau khi đăng nhập thành công thì chuyển qua đây */}
+            <Route path="/" element={<TestLoadapi />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
