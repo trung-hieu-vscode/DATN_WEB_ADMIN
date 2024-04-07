e0a0aa10defd57b38043ee5ba4fd6d3b8ec3082f
DThanh0 <115547764+DThanh0@users.noreply.github.com>
2024-04-07 15:43:58 +0700

    feat:chart_3m_or_1w

--- src/resoureces/components/pages/Dashboard.js
+++ src/resoureces/components/pages/Dashboard.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Modal, Button, Spinner, Navbar, FormControl, Form, ListGroup } from 'react-bootstrap';
+import { Button, Spinner, ListGroup, Dropdown } from 'react-bootstrap';
 import '../css/Dashboard.css';
 import AxiosInstance from '../../helper/Axiosintances';
 
@@ -10,64 +9,68 @@ const Dashboard = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [loading, setLoading] = useState(true);
   const [revenueData, setRevenueData] = useState([]);
+  const [timePeriod, setTimePeriod] = useState('3months');
+  const [selectedTimePeriod, setSelectedTimePeriod] = useState('3 Months');
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await AxiosInstance().get('/api/transaction/get_all_buy_vip');
+      if (response && response.data && Array.isArray(response.data)) {
+        const data = response.data.map(item => item.amount);
+        setRevenueData(data);
+      } else {
+        console.error('Error fetching revenue data');
+      }
+
+      const usersResponse = await AxiosInstance().get('/api/users');
+      if (usersResponse && usersResponse.users && Array.isArray(usersResponse.users)) {
+        const sortedUsers = usersResponse.users.sort((a, b) => b.balance - a.balance);
+        setUsers(sortedUsers);
+      } else {
+        console.error('Error fetching users');
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
 
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const response = await AxiosInstance().get('/api/transaction/get_all_buy_vip');
-        if (response && response.data && Array.isArray(response.data)) {
-          const data = response.data.map(item => item.amount);
-          setRevenueData(data);
-        } else {
-          console.error('Error fetching revenue data');
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
     fetchData();
   }, []);
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const response = await AxiosInstance().get('/api/users');
-        if (response && Array.isArray(response.users)) {
-          const sortedUsers = response.users.sort((a, b) => b.balance - a.balance);
-          setUsers(sortedUsers);
-        } else {
-          console.error('Error fetching users');
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);  
 
-  const getMonthNames = () => {
+  const getDates = (period) => {
     const date = new Date();
     const currentMonth = date.getMonth();
-    const formatter = new Intl.DateTimeFormat('en', { month: 'long' });
+    let dates = [];
+    if (period === '3months') {
+      const formatter = new Intl.DateTimeFormat('en', { month: 'long' });
+      dates = [
+        formatter.format(new Date(date.getFullYear(), currentMonth - 2, 1)),
+        formatter.format(new Date(date.getFullYear(), currentMonth - 1, 1)),
+        formatter.format(new Date(date.getFullYear(), currentMonth, 1))
+      ];
+    } else if (period === '1week') {
+      for (let i = 6; i >= 0; i--) {
+        dates.push(new Date(date.getFullYear(), currentMonth, date.getDate() - i).toLocaleDateString());
+      }
+    }
+    return dates;
+  };
 
-    return [
-      formatter.format(new Date(date.getFullYear(), currentMonth - 2, 1)),
-      formatter.format(new Date(date.getFullYear(), currentMonth - 1, 1)),
-      formatter.format(new Date(date.getFullYear(), currentMonth, 1)),
-    ];
+  const handleSelectTimePeriod = (period, label) => {
+    setTimePeriod(period);
+    setSelectedTimePeriod(label);
   };
 
   const lineChartData = {
-    labels: getMonthNames(),
+    labels: getDates(timePeriod),
     datasets: [
       {
-        label: 'dữ liệu',
+        label: 'Dữ liệu',
         backgroundColor: 'rgba(75,192,192,0.2)',
         borderColor: 'rgba(75,192,192,1)',
         borderWidth: 2,
@@ -98,24 +101,34 @@ const Dashboard = () => {
     return (
       <div className="text-center mt-5">
         <Spinner animation="border" role="status" />
-        <p className="mt-3">Đang tải bảng điều khiển...</p>
+        <p className="mt-3">Loading dashboard...</p>
       </div>
     );
   }
 
   return (
     <div className="container-fluid">
-      <h1 className="display-4 post-page-title">Bảng điều khiển</h1>
+      <h1 className="display-4 post-page-title">Dashboard</h1>
       <div style={{ height: '50px', backgroundColor: 'rgb(248, 249, 250)' }}></div>
       <div className="row">
         <div className="col-md-6">
-          <h2 className="text-center">Doanh thu </h2>
+          <h2 className="text-center">Thống kê</h2>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-time-period">
+              {selectedTimePeriod} {/* Sử dụng state để hiển thị khoảng thời gian được chọn */}
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => handleSelectTimePeriod('3months', '3 Months')}>3 Months</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleSelectTimePeriod('1week', '1 Week')}>1 Week</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
           <div className="chart-container">
             <Line data={lineChartData} options={lineChartOptions} />
           </div>
         </div>
         <div className="col-md-6">
-          <h2 className="text-center">Xếp hạng người nạp</h2>
+          <h2 className="text-center">Top Rechargers</h2>
           <div className="user-list-container">
             <ListGroup>
               {filteredUsers.map((user, index) => (
