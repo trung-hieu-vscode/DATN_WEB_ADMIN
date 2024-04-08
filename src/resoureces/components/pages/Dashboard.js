b63b8b892eb8590277084ce35040bbf952b0e72a
DThanh0 <115547764+DThanh0@users.noreply.github.com>
2024-04-08 23:05:30 +0700

    fix:vi and add more option chart

--- src/resoureces/components/pages/Dashboard.js
+++ src/resoureces/components/pages/Dashboard.js
@@ -10,7 +10,7 @@ const Dashboard = () => {
   const [loading, setLoading] = useState(true);
   const [revenueData, setRevenueData] = useState([]);
   const [timePeriod, setTimePeriod] = useState('3months');
-  const [selectedTimePeriod, setSelectedTimePeriod] = useState('3 Months');
+  const [selectedTimePeriod, setSelectedTimePeriod] = useState('3 Tháng');
 
   const fetchData = async () => {
     setLoading(true);
@@ -37,7 +37,6 @@ const Dashboard = () => {
     }
   };
 
-
   useEffect(() => {
     fetchData();
   }, []);
@@ -46,20 +45,35 @@ const Dashboard = () => {
     const date = new Date();
     const currentMonth = date.getMonth();
     let dates = [];
-    if (period === '3months') {
-      const formatter = new Intl.DateTimeFormat('en', { month: 'long' });
-      dates = [
-        formatter.format(new Date(date.getFullYear(), currentMonth - 2, 1)),
-        formatter.format(new Date(date.getFullYear(), currentMonth - 1, 1)),
-        formatter.format(new Date(date.getFullYear(), currentMonth, 1))
-      ];
-    } else if (period === '1week') {
-      for (let i = 6; i >= 0; i--) {
-        dates.push(new Date(date.getFullYear(), currentMonth, date.getDate() - i).toLocaleDateString());
-      }
+    const formatter = new Intl.DateTimeFormat('vi', { month: 'long' });
+  
+    switch (period) {
+      case '3months':
+        dates = [
+          formatter.format(new Date(date.getFullYear(), currentMonth - 2, 1)),
+          formatter.format(new Date(date.getFullYear(), currentMonth - 1, 1)),
+          formatter.format(new Date(date.getFullYear(), currentMonth, 1))
+        ];
+        break;
+      case '1month':
+        dates = [
+          formatter.format(new Date(date.getFullYear(), currentMonth - 1, 1)),
+          formatter.format(new Date(date.getFullYear(), currentMonth, 1))
+        ];
+        break;
+      case '3weeks':
+        for (let i = 20; i >= 0; i -= 7) {
+          dates.push(new Date(date.getFullYear(), currentMonth, date.getDate() - i).toLocaleDateString());
+        }
+        break;
+      case '1week':
+        for (let i = 6; i >= 0; i--) {
+          dates.push(new Date(date.getFullYear(), currentMonth, date.getDate() - i).toLocaleDateString());
+        }
+        break;
     }
     return dates;
-  };
+  };  
 
   const handleSelectTimePeriod = (period, label) => {
     setTimePeriod(period);
@@ -109,18 +123,19 @@ const Dashboard = () => {
   return (
     <div className="container-fluid">
       <h1 className="display-4 post-page-title">Dashboard</h1>
-      <div style={{ height: '50px', backgroundColor: 'rgb(248, 249, 250)' }}></div>
+      <div style={{ height: '50px', backgroundColor: 'rgb(248, 249, 250)'}}></div>
       <div className="row">
         <div className="col-md-6">
           <h2 className="text-center">Thống kê</h2>
           <Dropdown>
             <Dropdown.Toggle variant="success" id="dropdown-time-period">
-              {selectedTimePeriod} {/* Sử dụng state để hiển thị khoảng thời gian được chọn */}
+              {selectedTimePeriod}
             </Dropdown.Toggle>
-
             <Dropdown.Menu>
-              <Dropdown.Item onClick={() => handleSelectTimePeriod('3months', '3 Months')}>3 Months</Dropdown.Item>
-              <Dropdown.Item onClick={() => handleSelectTimePeriod('1week', '1 Week')}>1 Week</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleSelectTimePeriod('3months', '3 tháng')}>3 Tháng</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleSelectTimePeriod('1month', '1 tháng')}>1 Tháng</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleSelectTimePeriod('1week', '1 tuần')}>1 Tuần</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleSelectTimePeriod('3weeks', '3 tuần')}>3 Tuần</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
           <div className="chart-container">
@@ -128,7 +143,7 @@ const Dashboard = () => {
           </div>
         </div>
         <div className="col-md-6">
-          <h2 className="text-center">Top Rechargers</h2>
+          <h2 className="text-center">Xếp hạng nạp tiền</h2>
           <div className="user-list-container">
             <ListGroup>
               {filteredUsers.map((user, index) => (
