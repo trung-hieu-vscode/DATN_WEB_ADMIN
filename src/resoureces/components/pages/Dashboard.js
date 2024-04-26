3b05cb0c2693c370192743e8907051d73213cb12
DThanh0 <115547764+DThanh0@users.noreply.github.com>
2024-04-26 22:16:27 +0700

    [FEAT]add_chart_bar_select_startDate_endDate

--- src/resoureces/components/pages/Dashboard.js
+++ src/resoureces/components/pages/Dashboard.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { Button, Spinner, ListGroup, Dropdown } from 'react-bootstrap';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import '../css/Dashboard.css';
 import AxiosInstance from '../../helper/Axiosintances';
+import moment from 'moment';
 
 // Register the required components for Chart.js
 ChartJS.register(
   CategoryScale,
   LinearScale,
-  PointElement,
-  LineElement,
+  BarElement,
   Title,
   Tooltip,
   Legend
@@ -21,11 +24,10 @@ const Dashboard = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [loading, setLoading] = useState(true);
   const [revenueData, setRevenueData] = useState([]);
-  const [timePeriod, setTimePeriod] = useState('3months');
-  const [selectedTimePeriod, setSelectedTimePeriod] = useState('3 Tháng');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
   const fetchData = async () => {
-    setLoading(true);
     try {
       const response = await AxiosInstance().get('/api/transaction/get_all_buy_vip');
       if (response.data && Array.isArray(response.data)) {
@@ -44,82 +46,65 @@ const Dashboard = () => {
       }
     } catch (error) {
       console.error('Error fetching data:', error);
-    } finally {
-      setLoading(false);
     }
   };
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [startDate, endDate]);
 
-  const getDates = (period) => {
-    const date = new Date();
-    const currentMonth = date.getMonth();
-    let dates = [];
-    const formatter = new Intl.DateTimeFormat('vi', { month: 'long' });
-
-    switch (period) {
-      case '3months':
-        dates = [
-          formatter.format(new Date(date.getFullYear(), currentMonth - 2, 1)),
-          formatter.format(new Date(date.getFullYear(), currentMonth - 1, 1)),
-          formatter.format(new Date(date.getFullYear(), currentMonth, 1))
-        ];
-        break;
-      case '1month':
-        dates = [
-          formatter.format(new Date(date.getFullYear(), currentMonth - 1, 1)),
-          formatter.format(new Date(date.getFullYear(), currentMonth, 1))
-        ];
-        break;
-      case '3weeks':
-        for (let i = 20; i >= 0; i -= 7) {
-          dates.push(new Date(date.getFullYear(), currentMonth, date.getDate() - i).toLocaleDateString());
-        }
-        break;
-      case '1week':
-        for (let i = 6; i >= 0; i--) {
-          dates.push(new Date(date.getFullYear(), currentMonth, date.getDate() - i).toLocaleDateString());
-        }
-        break;
+  const getDates = () => {
+    let dateArray = [];
+    let currentDate = moment(startDate).startOf('day');
+    let end = moment(endDate).startOf('day');
+    while (currentDate <= end) {
+      dateArray.push(currentDate.format('DD-MM'));
+      currentDate = currentDate.add(1, 'days');
     }
-    return dates;
+    return dateArray;
   };
 
   function formatBalance(balance) {
     return new Intl.NumberFormat('de-DE').format(balance);
   }
 
-  const handleSelectTimePeriod = (period, label) => {
-    setTimePeriod(period);
-    setSelectedTimePeriod(label);
-  };
-
-  const lineChartData = {
-    labels: getDates(timePeriod),
+  const barChartData = {
+    labels: getDates(),
     datasets: [
       {
-        label: 'Dữ liệu',
-        backgroundColor: 'rgba(75,192,192,0.2)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderWidth: 2,
+        label: 'Revenue',
+        backgroundColor: 'rgba(0, 123, 255, 0.5)',
+        borderColor: 'rgba(0, 123, 255, 1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(0, 123, 255, 0.75)',
+        hoverBorderColor: 'rgba(0, 123, 255, 1)',
         data: revenueData,
-        fill: false,
       }
     ]
   };
 
-  const lineChartOptions = {
+  const barChartOptions = {
     responsive: true,
     scales: {
       x: {
-        type: 'category'
+        grid: {
+          display: false,
+        },
       },
       y: {
-        beginAtZero: true
+        beginAtZero: true,
       }
-    }
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        mode: 'index',
+        intersect: false,
+      },
+    },
+    maintainAspectRatio: false,
   };
 
   const filteredUsers = searchTerm.length === 0
@@ -145,24 +130,34 @@ const Dashboard = () => {
       <div className="row">
         <div className="col-md-6">
           <h2 className="text-center">Thống kê doanh thu</h2>
-          <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-time-period">
-              {selectedTimePeriod}
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item style={{fontSize:16}} onClick={() => handleSelectTimePeriod('3months', '3 tháng')}>3 tháng</Dropdown.Item>
-              <Dropdown.Item style={{fontSize:16}} onClick={() => handleSelectTimePeriod('1month', '1 tháng')}>1 tháng</Dropdown.Item>
-              <Dropdown.Item style={{fontSize:16}} onClick={() => handleSelectTimePeriod('1week', '1 tuần')}>1 tuần</Dropdown.Item>
-              <Dropdown.Item style={{fontSize:16}} onClick={() => handleSelectTimePeriod('3weeks', '3 tuần')}>3 tuần</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+          <div className="d-flex justify-content-around">
+            <DatePicker
+              selected={startDate}
+              onChange={date => setStartDate(date)}
+              selectsStart
+              startDate={startDate}
+              endDate={endDate}
+              className="date-picker"
+              dateFormat="dd-MM-yyyy"
+            />
+            <DatePicker
+              selected={endDate}
+              onChange={date => setEndDate(date)}
+              selectsEnd
+              startDate={startDate}
+              endDate={endDate}
+              minDate={startDate}
+              className="date-picker"
+              dateFormat="dd-MM-yyyy"
+            />
+          </div>
           <div className="chart-container">
-            <Line data={lineChartData} options={lineChartOptions} />
+            <Bar data={barChartData} options={barChartOptions} height={400} />
           </div>
         </div>
         <div className="col-md-6">
           <h2 className="text-center">Xếp hạng nạp tiền</h2>
-          <div className="user-list-container" style={{ maxHeight: '500px', overflowY: 'auto' }}>
+          <div className="user-list-container">
             <ListGroup>
               {filteredUsers.map((user, index) => (
                 <ListGroup.Item key={user._id || index}>
